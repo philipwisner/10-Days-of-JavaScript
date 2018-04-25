e9491b7a5063a5f8a0d8e006b3ce35623e5c4953
philipwisner <philipwisner@gmail.com>
2018-04-25 11:41:03 +0200

    refactor

--- Day 3 - Arrays.js
+++ Day 3 - Arrays.js
@@ -3,10 +3,9 @@
 //Complete the getSecondLargest function in the editor below. It has one parameter: nums an array, n, of  numbers. The function must find and return the second largest number in nums.
 
 function getSecondLargest(nums) {
-    let sortedArray = nums.sort((a, b) => a - b);
+    let sortedArray = nums.sort((a, b) => b - a);
     var uniqueArraySorted = sortedArray.filter(function(elem, index, self) {
         return index === self.indexOf(elem);
     })
-    let n = uniqueArraySorted.length - 2;
-    return uniqueArraySorted[n];
+    return uniqueArraySorted[1];
 }
