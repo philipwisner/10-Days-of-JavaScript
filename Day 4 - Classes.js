195974f3d20d8a8d8d3c442bc22331a8bc5c07bf
philipwisner <philipwisner@gmail.com>
2018-04-26 14:58:08 +0200

    Day 4 - 3

--- Day 4 - Classes.js
+++ Day 4 - Classes.js
@@ -1,16 +1,21 @@
 //DAY 4: Classes
 
-//Complete the function in the editor. It has two parameters: a and b. It must return an object modeling a rectangle that has the following properties:
+//Create a Polygon class that has the following properties:
 
-//length: This value is equal to a.
-//width: This value is equal to b.
-//perimeter: This value is equal to 2 * (a + b)
-//area: This value is equal to a * b
+//A constructor that takes an array of integer values describing the lengths of the polygon's sides.
+//A perimeter() method that returns the polygon's perimeter.
 
 
-function Rectangle(a, b) {
-    this.length = a;
-    this.width = b;
-    this.perimeter = 2 * (a + b);
-    this.area = a * b;
+class Polygon {
+	constructor(arr) {
+		this.sides = arr;
+	}
+	
+	perimeter() {
+		var perimeter = 0;
+        this.sides.forEach(function(side) {
+            perimeter += side;
+        });
+        return perimeter
+	}
 }
